61fe03f2c7757a5c4d3ad3318c971a91e4f258c3
/Pratik <67191233+pratik12350@users.noreply.github.com>
2022-01-15 17:02:57 +0000

    E

--- index.js
+++ index.js
@@ -29,8 +29,13 @@ client.on('messageCreate', (message) => {
     let embed = new MessageEmbed()
     .setTitle("HELLO!")
     .setDescription(`<:Yveltal:841132745278226442> Hello ${message.author}!\n<:Yveltal:841132745278226442> Im Poketwo Helper For ${message.guild.name}\n<:Yveltal:841132745278226442> Prefix: \`y?\``)
-
-    message.channel.send({ embeds: [embed] })
+      /*
+      .addField("How To Use Tags?", "There are 3 Category of tags.\n\n> stats\n\n> moveset\n\n> info\n\nUse command in `y?<pokemon>-<category>` this syntax!")
+  */
+      .setThumbnail("https://cdn.discordapp.com/emojis/844156428309495809.png")
+    .setFooter("Use y?tag-help to learn about tags!")
+.setColor("RED")
+        message.channel.send({ embeds: [embed] })
   }
   
 })
